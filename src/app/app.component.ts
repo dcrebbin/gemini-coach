8be09767f6b0594bd952a928924a1ecc5465c159
Devon Crebbin <devon@artvuu.group>
2024-02-04 12:58:45 +1100

    added initial interactivity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GeminiService } from './services/gemini.service';
+import {
+  FormBuilder,
+  FormControl,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Your Coach 🤝';
+  geminiForm = new FormBuilder().group({
+    geminiInput: new FormControl(''),
+  });
+  constructor(private geminiService: GeminiService) {
+    console.log('AppComponent.constructor()');
+  }
+  public sendMessage() {
+    const message = this.geminiForm.get('geminiInput')?.value?.toString() ?? '';
+    if (message === '') {
+      return;
+    }
+    this.geminiService.sendMessage(message);
+  }
 }
