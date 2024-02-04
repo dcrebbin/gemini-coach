7233f36e7766902a7394906088ae48536e68b5da
Devon Crebbin <devon@artvuu.group>
2024-02-04 15:58:28 +1100

    added recording interactivity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GeminiService } from './services/gemini.service';
 import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
-import { NgFor, NgIf } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, NgFor, NgIf, MatIconModule],
+  imports: [
+    RouterOutlet,
+    ReactiveFormsModule,
+    NgFor,
+    NgIf,
+    MatIconModule,
+    NgClass,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -33,10 +40,47 @@ export class AppComponent {
 
   public speakMessage(index: number) {
     alert('Speaking message: ' + this.conversation[index].message);
+    const speech = new SpeechSynthesisUtterance(
+      this.conversation[index].message
+    );
+    speech.lang = 'en-US';
+    window.speechSynthesis.speak(speech);
+  }
+
+  mediaRecorder: any;
+
+  microphoneOnClass: string =
+    'absolute right-0 p-2 mx-2 flex items-center text-red-500';
+  microphoneOffClass: string = 'absolute right-0 p-2 mx-2 flex items-center';
+  microphoneClass: string = this.microphoneOffClass;
+  isRecording = false;
+
+  public interactWithMicrophone() {
+    if (this.isRecording) {
+      this.stopAudio();
+      this.isRecording = false;
+    } else {
+      this.recordAudio();
+      this.isRecording = true;
+    }
   }
 
   public recordAudio() {
-    alert('Recording audio...');
+    this.microphoneClass = this.microphoneOnClass;
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      this.mediaRecorder = new MediaRecorder(stream);
+      this.mediaRecorder.start();
+      this.mediaRecorder.ondataavailable = (e: any) => {
+        const audioURL = URL.createObjectURL(e.data);
+        const audio = new Audio(audioURL);
+        audio.play();
+      };
+    });
+  }
+
+  public stopAudio() {
+    this.microphoneClass = this.microphoneOffClass;
+    this.mediaRecorder.stop();
   }
 
   public sendMessage() {
