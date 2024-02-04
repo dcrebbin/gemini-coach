90797854f716bba33521f6ac9ea0cb9b07f25725
Devon Crebbin <devon@artvuu.group>
2024-02-04 13:14:53 +1100

    added updating conversation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GeminiService } from './services/gemini.service';
-import {
-  FormBuilder,
-  FormControl,
-  ReactiveFormsModule,
-} from '@angular/forms';
+import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule],
+  imports: [RouterOutlet, ReactiveFormsModule, NgFor,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
+
+// interface Message{
+//   message: string;
+//   from: string;
+// }
 export class AppComponent {
   geminiForm = new FormBuilder().group({
     geminiInput: new FormControl(''),
   });
+
   constructor(private geminiService: GeminiService) {
     console.log('AppComponent.constructor()');
   }
+
+  conversation: any[] = [
+    { message: 'Hey Devon, how are you today? 😁', from: 'gemini' },
+    { message: 'Not too bad but feeling a bit stressed lately', from: 'user' },
+    {
+      message: `Sorry to hear that Devon 😥
+      Would you like to talk about it?`,
+      from: 'gemini',
+    },
+  ];
+
   public sendMessage() {
     const message = this.geminiForm.get('geminiInput')?.value?.toString() ?? '';
     if (message === '') {
       return;
     }
+    this.conversation.push({ message, from: 'user' });
     this.geminiService.sendMessage(message);
   }
 }
